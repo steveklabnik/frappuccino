8f809dcfb312baf6a5ce007170e3a3f91dbaff23
Steve Klabnik <steve@steveklabnik.com>
2013-04-23 11:53:08 +0200

    Another merge test.
    This time, testing +1/-1. Neat.

--- test/merge_test.rb
+++ test/merge_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 
+class PlusOneButton
+  def push
+    emit(:plus_one)
+  end
+end
+
+class MinusOneButton
+  def push
+    emit(:minus_one)
+  end
+end
+
 describe "merging steams" do
   it "produces one stream with both sets of events" do
     button_one = Button.new
@@ -16,6 +28,46 @@ describe "merging steams" do
     button_one.push
     button_two.push
 
+    assert_equal 2, counter.to_i
+  end
+
+  it "+1/-1" do
+    plus_button = PlusOneButton.new
+    minus_button = MinusOneButton.new
+
+    stream_one = Frappuccino::Stream.new(plus_button)
+    stream_two = Frappuccino::Stream.new(minus_button)
+
+    merged_stream = Frappuccino::Stream.merge(stream_one, stream_two)
+    counter = merged_stream
+              .map do |event|
+                case event
+                when :plus_one
+                  1
+                when :minus_one
+                  -1
+                else
+                  0
+                end
+              end
+              .inject(0) {|sum, n| sum + n }
+
+    assert_equal 0, counter.to_i
+
+    plus_button.push
+    assert_equal 1, counter.to_i
+
+    minus_button.push
+    assert_equal 0, counter.to_i
+
+    minus_button.push
+    minus_button.push
+    assert_equal -2, counter.to_i
+
+    plus_button.push
+    plus_button.push
+    plus_button.push
+    plus_button.push
     assert_equal 2, counter.to_i
   end
 end
