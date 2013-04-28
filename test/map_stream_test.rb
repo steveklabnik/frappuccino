58570cca4f766d1ae6a009468cf8ce190187558e
Steve Klabnik <steve@steveklabnik.com>
2013-04-27 18:10:41 -0700

    add test case for default value.
    Thanks coveralls.

--- test/map_stream_test.rb
+++ test/map_stream_test.rb
@@ -20,4 +20,23 @@ describe "#map_stream" do
     plus_button.push
     assert_equal 1, counter.to_i
   end
+
+  it "respects the default value of the hash" do
+    plus_button = PlusOneButton.new
+    minus_button = MinusOneButton.new
+
+    stream_one = Frappuccino::Stream.new(plus_button)
+    stream_two = Frappuccino::Stream.new(minus_button)
+
+    merged_stream = Frappuccino::Stream.merge(stream_one, stream_two)
+    counter = merged_stream
+              .map_stream(:default => 1)
+              .inject(0) {|sum, n| sum + n }
+
+    assert_equal 0, counter.to_i
+
+    plus_button.push
+    minus_button.push
+    assert_equal 2, counter.to_i
+  end
 end
